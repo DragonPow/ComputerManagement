de37a3ce4d59f8360e929082ee1c87e8c620a922
UIT.19520266 <19520266@gm.uit.edu.vn>
2021-10-22 17:24:32 +0700

    remove CumputerProject.CustomerWorkspace

--- ComputerProject/Repository/CategoryRepository.cs
+++ ComputerProject/Repository/CategoryRepository.cs
@@ -72,7 +72,7 @@ namespace ComputerProject.Repository
         {
             using (var db = new ComputerManagementEntities())
             {
-
+                //TODO: save category and child, specification
             }
         }
 
