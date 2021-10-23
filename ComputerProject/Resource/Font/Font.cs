f101941f438d1cb1ca0ab6c654d54ac6edbcc5d2
HuynhThiMinhNhuc <huynhthiminhnhuc@gmail.com>
2021-10-23 16:10:33 +0700

    create

---
+++ ComputerProject/Resource/Font/Font.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ComputerProject.Resource.Font
+{
+    public static class Font
+    {
+        public static double Title = 14;
+        public static double Body = 38;
+    }
+}
