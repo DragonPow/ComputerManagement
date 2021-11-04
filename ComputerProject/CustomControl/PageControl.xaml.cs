f48fa02eeab33d2bf7912d005ce500467de9aacf
HuynhThiMinhNhuc <huynhthiminhnhuc@gmail.com>
2021-11-04 23:10:20 +0700

    Update StatiticUI

--- ComputerProject/CustomControl/PageControl.xaml.cs
+++ ComputerProject/CustomControl/PageControl.xaml.cs
@@ -25,8 +25,8 @@ namespace ComputerProject.CustomControl
             InitializeComponent();
         }
 
-        private static readonly DependencyProperty CurrentPageProperties = DependencyProperty.Register(nameof(CurrentPage), typeof(int), typeof(PageControl), new PropertyMetadata(1000));
-        private static readonly DependencyProperty MaxPageProperties = DependencyProperty.Register(nameof(MaxPage), typeof(int), typeof(PageControl), new PropertyMetadata(1));
+        private static readonly DependencyProperty CurrentPageProperties = DependencyProperty.Register(nameof(CurrentPage), typeof(int), typeof(PageControl), new PropertyMetadata(1));
+        private static readonly DependencyProperty MaxPageProperties = DependencyProperty.Register(nameof(MaxPage), typeof(int), typeof(PageControl), new PropertyMetadata(1000));
 
 
         public int CurrentPage
