179e0b3d3af35993001b9c8907945840a30972d3
UIT.19520266 <19520266@gm.uit.edu.vn>
2021-11-16 01:11:26 +0700

    binding to view

--- ComputerProject/Repository/BillRepository.cs
+++ ComputerProject/Repository/BillRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
@@ -22,24 +23,36 @@ namespace ComputerProject.Repository
             if (timeFrom.HasValue) query = query.Where(i => i.createTime.Year >= timeFrom.Value.Year && i.createTime.Month >= timeFrom.Value.Month && i.createTime.Day >= timeFrom.Value.Day);
             if (timeTo.HasValue) query = query.Where(i => i.createTime.Year <= timeTo.Value.Year && i.createTime.Month <= timeTo.Value.Month && i.createTime.Day <= timeTo.Value.Day);
 
-            return query;
+            return query.OrderBy(i=>i.createTime);
         }
         /// <summary>
         /// Load bill from database
         /// </summary>
-        /// <param name="maxNumberInPage">Max number of bill in one page</param>
+        /// <param name="maxNumberInPage">Max number of bill in one page, if this equal 0, get all bill</param>
         /// <param name="numberPage">Index of page, start from 1</param>
         /// <param name="text">Search page by name</param>
         /// <param name="timeFrom"></param>
         /// <param name="timeTo"></param>
-        /// <returns></returns>
+        /// <returns>Collection bill with filter</returns>
         public Collection<BILL> LoadBills(int maxNumberInPage, int numberPage = 1, string text = null, DateTime? timeFrom = null, DateTime? timeTo = null)
         {
-            //query = query.Skip((numberPage - 1) * maxPage).Take(numberPage);
-            return null;
+            if (maxNumberInPage < 0 || numberPage < 0) throw new ArgumentException("The number is larger than 0");
+            Collection<BILL> bills = null;
+
+            using (var db = new ComputerManagementEntities())
+            {
+                var query = CreateQuery(db, text, timeFrom, timeTo);
+                if (maxNumberInPage > 0) query = query.Skip((numberPage - 1) * maxNumberInPage).Take(maxNumberInPage);
+
+                var list = query.Include(i => i.CUSTOMER).AsEnumerable();
+
+                bills = new ObservableCollection<BILL>(list);
+            }
+
+            return bills;
         }
 
-        public Collection<BILL> LoadBills(int numberPage, int maxNumberInPage)
+        public Collection<BILL> LoadBills(int maxNumberInPage, int numberPage = 1)
         {
             return LoadBills(maxNumberInPage, numberPage, null, null, null);
         }
