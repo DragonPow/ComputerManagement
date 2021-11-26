9ed8ecab782ff27561ef52e075e85d79d7524c24
thuytien192 <vothithuytien.1902@gmail.com>
2021-11-26 22:44:14 +0700

    Fix UI

--- ComputerProject/Resource/Font/Font.cs
+++ ComputerProject/Resource/Font/Font.cs
@@ -13,6 +13,6 @@ namespace ComputerProject.Resource.Font
         public static double Body = 14;
         public static double SmallBody = 12;
         public static double TitleView = 24;
-        public static double SubTitle = 22;
+        public static double SubTitle = 20;
     }
 }
